18d6c1e75ce4e3ff899c9f58e47da6695a73477f
AswinTom Varghese <aswin.thunchathu@gmail.com>
2020-04-11 09:25:43 +0200

    updated animation

--- src/Components/Skeleton.svelte
+++ src/Components/Skeleton.svelte
@@ -35,30 +35,54 @@
     border: 1px solid #111;
     width: 100%;
     z-index: 999;
+    border: 1px solid rgb(34, 33, 33);
   }
 
-  .skeleton-box-inner::after {
+  .skeleton-box-inner::after,
+  .skeleton-box-inner::before {
     position: absolute;
     top: 0;
     right: 0;
     bottom: 0;
     left: 0;
     content: "";
+  }
+
+  .skeleton-box-inner::after {
     transform: translateX(-100%);
-    animation: shimmer 0.8s infinite;
+    animation: shimmer 1s infinite;
     background-image: linear-gradient(
       90deg,
-      rgba(0,0,0, 0) 0,
-      rgba(0,0,0, 0.2) 20%,
-      rgba(0,0,0, 0.3) 60%,
-      rgba(0,0,0, 0.2) 80%,
-      rgba(0,0,0, 0)
+      rgba(0, 0, 0, 0) 0,
+      rgba(0, 0, 0, 0.2) 20%,
+      rgba(0, 0, 0, 0.3) 60%,
+      rgba(39, 39, 39, 0.2) 80%,
+      rgba(39, 39, 39, 0.3)
     );
+  }
+
+  .skeleton-box-inner::before {
+    animation: blink 0.5s infinite;
+    background-color: rgba(39, 39, 39, 0.5);
   }
 
   @keyframes shimmer {
     100% {
       transform: translateX(100%);
+    }
+  }
+
+  @keyframes blink {
+    0% {
+      opacity: 0.5;
+    }
+
+    50% {
+      opacity: 1;
+    }
+
+    100% {
+      opacity: 0.5;
     }
   }
 </style>
